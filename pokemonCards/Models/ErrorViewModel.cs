8c9ccaadaa1ea322359439341d2808a2d549da3f
foley179 <81254761+foley179@users.noreply.github.com>
2022-06-26 17:10:33 +0100

    Add project files.

---
+++ pokemonCards/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pokemonCards.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
