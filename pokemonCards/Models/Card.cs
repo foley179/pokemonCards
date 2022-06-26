ac73ea1197d1ecaa21dcdee3898cc8321b8de5e0
foley179 <81254761+foley179@users.noreply.github.com>
2022-06-26 17:37:12 +0100

    add user and card

---
+++ pokemonCards/Models/Card.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace pokemonCards.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "User")]
+        public string User { get; set; }
+        //public IEnumerable<SelectListItem> UserList { get; set; }
+        // currently causing error
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Serial Code")]
+        public string Serial { get; set; }
+
+        public Card()
+        {
+            // left empty for other methods
+        }
+    }
+}
---
+++ pokemonCards/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pokemonCards.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string Name { get; set; }
+
+        public User()
+        {
+            // left empty for other methods
+        }
+    }
+}
