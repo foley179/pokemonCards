2f15f1375e3c11029b69c8831a39f2dd5845af9f
foley179 <81254761+foley179@users.noreply.github.com>
2022-06-30 14:08:45 +0100

    populate select fixed, edit button listener added

--- pokemonCards/wwwroot/js/site.js
+++ pokemonCards/wwwroot/js/site.js
@@ -9,6 +9,22 @@ if ($(location).attr("pathname") == "/Cards/Create") {
         type: "GET",
         url: "/Users/GetUsers",
         success: function (data) {
+
+            // clear select
+            $("#userSelect").empty();
+
+            // add empty option
+            $("#userSelect").append(
+                $("<option>", {
+                    value: "",
+                    disabled: true,
+                    default: true,
+                    text: "----- Select User -----",
+                    class: "text-secondary"
+                })
+            );
+
+            // populate select
             for (var i = 0; i < data.length; i++) {
                 $("#userSelect").append(
                     $("<option>", {
@@ -24,4 +40,12 @@ if ($(location).attr("pathname") == "/Cards/Create") {
 $(".imgButton").on("click", (e) => {
     e.preventDefault();
     $("#imgModal").modal("show");
+});
+
+$("#editSaveButton").on("click", (e) => {
+    console.log("test");
+    e.preventDefault();
+    let user = $("#userOption").val();
+    $("#userContainer").text(user);
+    $("#confirmModal").modal("show");
 });
